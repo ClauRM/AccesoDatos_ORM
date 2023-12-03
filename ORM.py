66cac83487ae980b03b228b5f7a18dbbc5604420
LAPTOP-I7228BSL\claud <claudiaesther.rubio@gmail.com>
2023-12-03 18:43:33 +0100

    Posicionar aleatoriamente a la Persona utilizando la librería random

--- ORM.py
+++ ORM.py
@@ -1,15 +1,23 @@
 import tkinter as tk
+import random
 
 class Persona:
     def __init__(self):
-        self.posx = 512 #posicion
-        self.posy = 512
+        self.posx = random.randint(0,350) #posicion 350 es la mitad del lienzo
+        self.posy = random.randint(0,350)
+        self.radio = 20
     def dibuja(self):
-        lienzo.create_oval(30,30,60,60,fill="red")
+        lienzo.create_oval(
+            self.posx-self.radio/2,
+            self.posy-self.radio/2,
+            self.posx+self.radio/2,
+            self.posy+self.radio/2,
+            fill="red")
+            #para centrar el ovalo en el lienzo
 
 ventana = tk.Tk()
 
-lienzo = tk.Canvas(width=1024,height=1024)
+lienzo = tk.Canvas(width=700,height=700)
 lienzo.pack()
 
 persona = Persona()
